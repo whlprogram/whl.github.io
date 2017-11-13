b66672bd580d4368a7a1a5a22fdb193f09a70fef
王海龙 <2451774200@qq.com>
2017-11-14 05:12:00 +0800

    2017.11.14
    数据分析平台v.1.0

---
+++ template/assets/js/charts/GZMapChart.js
@@ -0,0 +1,338 @@
+﻿var dom = document.getElementById("GZQD");
+var myChart = echarts.init(dom);
+var app = {};
+option = null;
+
+var data = [
+    {name: '拉萨', value: 84},
+    {name: '上海', value: 65},
+    {name: '广州', value: 78},
+    {name: '太原', value: 59},   
+    {name: '昆明', value: 49},      
+    {name: '沈阳', value: 40},    
+    {name: '长春', value: 31},   
+    {name: '银川', value: 72},    
+    {name: '南昌', value: 24},    
+    {name: '吉林', value: 46},   
+    {name: '呼和浩特', value: 65},
+    {name: '成都', value: 18},    
+    {name: '西安', value: 31},    
+    {name: '重庆', value: 23},    
+    {name: '南京', value: 67},    
+    {name: '贵阳', value: 31},    
+    {name: '北京', value: 74},    
+    {name: '乌鲁木齐', value: 84},    
+    {name: '杭州', value: 80},   
+    {name: '济南', value: 72},   
+    {name: '兰州', value: 69},  
+    {name: '天津', value: 75},  
+    {name: '郑州', value: 73},
+    {name: '哈尔滨', value: 74},  
+    {name: '石家庄', value: 57},
+    {name: '长沙', value: 65},
+    {name: '合肥', value: 79},
+    {name: '武汉', value: 53},
+];
+
+var geoCoordMap = { 
+    '拉萨':[91.11,29.97],
+    '上海':[121.48,31.22],
+    '广州':[113.23,23.16],
+    '太原':[112.53,37.87],
+    '昆明':[102.73,25.04],
+    '沈阳':[123.38,41.8],
+    '长春':[125.35,43.88],
+    '银川':[106.27,38.47],  
+    '南昌':[115.89,28.68],    
+    '吉林':[126.57,43.87],   
+    '呼和浩特':[111.65,40.82],
+    '成都':[104.06,30.67],   
+    '西安':[108.95,34.27],   
+    '重庆':[106.54,29.59],  
+    '南京':[118.78,32.04],
+    '贵阳':[106.71,26.57],
+    '北京':[116.46,39.92],
+    '乌鲁木齐':[87.68,43.77],
+    '杭州':[120.19,30.26],  
+    '济南':[117,36.65],
+    '兰州':[103.73,36.03],
+    '天津':[117.2,39.13],
+    '郑州':[113.65,34.76],
+    '哈尔滨':[126.63,45.75],
+    '石家庄':[114.48,38.03],
+    '长沙':[113,28.21],
+    '合肥':[117.27,31.86],
+    '武汉':[114.31,30.52]
+};
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var geoCoord = geoCoordMap[data[i].name];
+        if (geoCoord) {
+            res.push({
+                name: data[i].name,
+                value: geoCoord.concat(data[i].value)
+            });
+        }
+    }
+    return res;
+};
+
+function renderItem(params, api) {
+    var coords = [
+        [116.7,39.53],
+        [103.73,36.03],
+        [112.91,27.87],
+        [120.65,28.01],
+        [119.57,39.95]
+    ];
+    var points = [];
+    for (var i = 0; i < coords.length; i++) {
+        points.push(api.coord(coords[i]));
+    }
+    var color = api.visual('color');
+
+    return {
+        type: 'polygon',
+        shape: {
+            points: echarts.graphic.clipPointsByRect(points, {
+                x: params.coordSys.x,
+                y: params.coordSys.y,
+                width: params.coordSys.width,
+                height: params.coordSys.height
+            })
+        },
+        style: api.style({
+            fill: color,
+            stroke: echarts.color.lift(color)
+        })
+    };
+}
+
+option = {
+    backgroundColor: '#404a59',
+    title: {
+        text: '全国光照资源分布',
+        subtext: '花草云智能监控',
+        sublink: '###',
+        left: 'center',
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    tooltip : {
+        trigger: 'item'
+    },
+    bmap: {
+        center: [104.114129, 37.550339],
+        zoom: 5,
+        roam: true,
+        mapStyle: {
+            styleJson: [
+                    {
+                        "featureType": "water",
+                        "elementType": "all",
+                        "stylers": {
+                            "color": "#044161"
+                        }
+                    },
+                    {
+                        "featureType": "land",
+                        "elementType": "all",
+                        "stylers": {
+                            "color": "#004981"
+                        }
+                    },
+                    {
+                        "featureType": "boundary",
+                        "elementType": "geometry",
+                        "stylers": {
+                            "color": "#064f85"
+                        }
+                    },
+                    {
+                        "featureType": "railway",
+                        "elementType": "all",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "highway",
+                        "elementType": "geometry",
+                        "stylers": {
+                            "color": "#004981"
+                        }
+                    },
+                    {
+                        "featureType": "highway",
+                        "elementType": "geometry.fill",
+                        "stylers": {
+                            "color": "#005b96",
+                            "lightness": 1
+                        }
+                    },
+                    {
+                        "featureType": "highway",
+                        "elementType": "labels",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "arterial",
+                        "elementType": "geometry",
+                        "stylers": {
+                            "color": "#004981"
+                        }
+                    },
+                    {
+                        "featureType": "arterial",
+                        "elementType": "geometry.fill",
+                        "stylers": {
+                            "color": "#00508b"
+                        }
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "all",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "green",
+                        "elementType": "all",
+                        "stylers": {
+                            "color": "#056197",
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "subway",
+                        "elementType": "all",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "manmade",
+                        "elementType": "all",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "local",
+                        "elementType": "all",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "arterial",
+                        "elementType": "labels",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    },
+                    {
+                        "featureType": "boundary",
+                        "elementType": "geometry.fill",
+                        "stylers": {
+                            "color": "#029fd4"
+                        }
+                    },
+                    {
+                        "featureType": "building",
+                        "elementType": "all",
+                        "stylers": {
+                            "color": "#1a5787"
+                        }
+                    },
+                    {
+                        "featureType": "label",
+                        "elementType": "all",
+                        "stylers": {
+                            "visibility": "off"
+                        }
+                    }
+            ]
+        }
+    },
+    series : [
+        {
+            name: 'pm2.5',
+            type: 'scatter',
+            coordinateSystem: 'bmap',
+            data: convertData(data),
+            symbolSize: function (val) {
+                return val[2] / 10;
+            },
+            label: {
+                normal: {
+                    formatter: '{b}',
+                    position: 'right',
+                    show: false
+                },
+                emphasis: {
+                    show: true
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#ddb926'
+                }
+            }
+        },
+        {
+            name: 'Top 5',
+            type: 'effectScatter',
+            coordinateSystem: 'bmap',
+            data: convertData(data.sort(function (a, b) {
+                return b.value - a.value;
+            }).slice(0, 6)),
+            symbolSize: function (val) {
+                return val[2] / 10;
+            },
+            showEffectOn: 'emphasis',
+            rippleEffect: {
+                brushType: 'stroke'
+            },
+            hoverAnimation: true,
+            label: {
+                normal: {
+                    formatter: '{b}',
+                    position: 'right',
+                    show: true
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#f4e925',
+                    shadowBlur: 10,
+                    shadowColor: '#333'
+                }
+            },
+            zlevel: 1
+        },
+        {
+            type: 'custom',
+            coordinateSystem: 'bmap',
+            //renderItem: renderItem,
+            itemStyle: {
+                normal: {
+                    opacity: 0.5
+                }
+            },
+            animation: false,
+            silent: true,
+            data: [0],
+            z: -10
+        }
+    ]
+};;
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}
---
+++ template/assets/js/charts/InlineChart.js
@@ -0,0 +1,511 @@
+﻿/*----------------------折线图-----------------------*/
+//折线图堆叠
+(function(){
+	
+	var myChart = echarts.init(document.getElementById("Stack"));
+	
+	option = {
+    title: {
+        text: '室内光照'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['室内光照强度']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['4:00','6:00','8:00','10:00','12:00','14:00','16:00','18:00','20:00','22:00','24:00']
+    },
+    yAxis: {
+        type: 'value'
+		//axisLabel : {
+        //    formatter: '{value} °C'
+        //}
+		//min: 5;
+		//max: 50;
+    },
+    series: [
+        {
+            name:'室内光照强度',
+            type:'line',
+            stack: '总量',
+            data:[0, 28, 38, 47, 58, 63, 53, 35, 20, 20, 0]
+        }
+    ]
+};
+
+myChart.setOption(option);
+})();
+
+
+//堆叠区域图
+(function(){
+	
+	var area = echarts.init(document.getElementById("area"));
+	
+	option = {
+    title: {
+        text: '土壤相对湿度'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['湿度百分比']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['6:00','8:00','10:00','12:00','14:00','浇水','18:00','20:00','22:00','24:00']
+    },
+    yAxis: {
+        type: 'value'
+		//axisLabel : {
+        //    formatter: '{value} °C'
+        //}
+		//min: 5;
+		//max: 50;
+    },
+    series: [
+        {
+            name:'湿度百分比',
+            type:'line',
+            stack: '总量',
+            data:[77, 76, 70, 66, 63, 53, 76, 71, 70, 68]
+        }
+    ]
+};
+
+area.setOption(option);
+})();
+
+
+//对数轴
+(function(){
+	
+	var step = echarts.init(document.getElementById("step"));
+	
+	option = {
+    title: {
+        text: '室内温度'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['室内温度']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['4:00','6:00','8:00','10:00','12:00','14:00','16:00','18:00','20:00','22:00','24:00']
+    },
+    yAxis: {
+        type: 'value'
+		//axisLabel : {
+        //    formatter: '{value} °C'
+        //}
+		//min: 5;
+		//max: 50;
+    },
+    series: [
+        {
+            name:'室内温度',
+            type:'line',
+            stack: '总量',
+            data:[7, 11, 13, 15, 18, 20, 19, 16, 12, 11, 10]
+        }
+    ]
+};
+
+step.setOption(option);
+})();
+
+
+//大数据量面积图
+(function(){
+	var shuju = echarts.init(document.getElementById("shuju"));
+	
+	option = {
+    title: {
+        text: '土壤微量元素'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['N+','P2O5','K2O','Mg+']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一','周二','周三','周四','周五','周六','周日']
+    },
+    yAxis: {
+        type: 'value'
+		//axisLabel : {
+        //    formatter: '{value} °C'
+        //}
+		//min: 5;
+		//max: 50;
+    },
+    series: [
+        {
+            name:'N+',
+            type:'line',
+            stack: '总量',
+            data:[0.21, 0.22, 0.17, 0.18, 0.15, 0.14, 0.13]
+        },
+        {
+            name:'P2O5',
+            type:'line',
+            stack: '总量',
+            data:[0.29, 0.28, 0.29, 0.35, 0.28, 0.26, 0.25]
+        },
+        {
+            name:'K2O',
+            type:'line',
+            stack: '总量',
+            data:[0.11, 0.12, 0.13, 0.18, 0.15, 0.14, 0.13]
+        },
+        {
+			name:'Mg+',
+            type:'line',
+            stack: '总量',
+            data:[0.05, 0.05, 0.05, 0.05, 0.04, 0.04, 0.04]
+        }
+    ]
+};
+
+	
+	shuju.setOption(option);
+})();
+
+
+
+/*----------------------柱状图-----------------------*/
+//坐标轴刻度与标签对齐
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////柱状图
+//(function(){
+//	
+//var myChart = echarts.init(document.getElementById("histogram"));
+//
+//option = {
+//	
+//	title: {
+//		text: "柱状图",
+//		x:'center'
+//	},
+//	
+//  color: ['#3398DB'],
+//  tooltip : {
+//      trigger: 'axis',
+//      axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+//          type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+//      }
+//  },
+//  toolbox: {
+//      feature: {
+//          saveAsImage: {}
+//      }
+//  },
+//  grid: {
+//      left: '3%',
+//      right: '4%',
+//      bottom: '3%',
+//      containLabel: true
+//  },
+//  xAxis : [
+//      {
+//          type : 'category',
+//          data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+//          axisTick: {
+//              alignWithLabel: true
+//          }
+//      }
+//  ],
+//  yAxis : [
+//      {
+//          type : 'value'
+//      }
+//  ],
+//  series : [
+//      {
+//          name:'直接访问',
+//          type:'bar',
+//          barWidth: '60%',
+//          data:[10, 52, 200, 334, 390, 330, 220]
+//      }
+//  ]
+//};
+//
+//myChart.setOption(option);
+//})();
+//
+//
+////折线图
+//(function(){
+//	
+//	var line = echarts.init(document.getElementById("line"));
+//	
+//	option = {
+//  title: {
+//      text: '堆叠区域图',
+//      x:'center'
+//  },
+//  tooltip : {
+//      trigger: 'axis'
+//  },
+//  
+//  toolbox: {
+//      feature: {
+//          saveAsImage: {}
+//      }
+//  },
+//  grid: {
+//      left: '3%',
+//      right: '4%',
+//      bottom: '3%',
+//      containLabel: true
+//  },
+//  xAxis : [
+//      {
+//          type : 'category',
+//          boundaryGap : false,
+//          data : ['周一','周二','周三','周四','周五','周六','周日']
+//      }
+//  ],
+//  yAxis : [
+//      {
+//          type : 'value'
+//      }
+//  ],
+//  series : [
+//      
+//      {
+//          name:'联盟广告',
+//          type:'line',
+//          stack: '总量',
+//          areaStyle: {normal: {}},
+//          data:[220, 182, 191, 234, 290, 330, 310]
+//      },
+//      {
+//          name:'视频广告',
+//          type:'line',
+//          stack: '总量',
+//          areaStyle: {normal: {}},
+//          data:[150, 232, 201, 154, 190, 330, 410]
+//      },
+//      {
+//          name:'直接访问',
+//          type:'line',
+//          stack: '总量',
+//          areaStyle: {normal: {}},
+//          data:[320, 332, 301, 334, 390, 330, 320]
+//      },
+//      {
+//          name:'搜索引擎',
+//          type:'line',
+//          stack: '总量',
+//          label: {
+//              normal: {
+//                  show: true,
+//                  position: 'top'
+//              }
+//          },
+//          areaStyle: {normal: {}},
+//          data:[820, 932, 901, 934, 1290, 1330, 1320]
+//      }
+//  ]
+//};
+//
+//line.setOption(option);
+//})();
+//
+//
+////饼状图
+//(function(){
+//
+//	var pie = echarts.init(document.getElementById("pie"));
+//	
+//	option = {
+//  title : {
+//      text: '饼状图',
+//      subtext: '纯属虚构',
+//      x:'center'
+//  },
+//  tooltip : {
+//      trigger: 'item',
+//      formatter: "{a} <br/>{b} : {c} ({d}%)"
+//  },
+//  toolbox: {
+//      feature: {
+//          saveAsImage: {}
+//      }
+//  },
+//  legend: {
+//      orient: 'vertical',
+//      left: 'left',
+//      data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+//  },
+//  series : [
+//      {
+//          name: '访问来源',
+//          type: 'pie',
+//          radius : '55%',
+//          center: ['50%', '60%'],
+//          data:[
+//              {value:335, name:'直接访问'},
+//              {value:310, name:'邮件营销'},
+//              {value:234, name:'联盟广告'},
+//              {value:135, name:'视频广告'},
+//              {value:1548, name:'搜索引擎'}
+//          ],
+//          itemStyle: {
+//              emphasis: {
+//                  shadowBlur: 10,
+//                  shadowOffsetX: 0,
+//                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+//              }
+//          }
+//      }
+//  ]
+//};
+//
+//
+//pie.setOption(option);
+//})();
+//
+//
+////环形图
+//(function(){
+//	
+//	var annular = echarts.init(document.getElementById("annular"));
+//	
+//
+//	option = {
+//		
+//		title: {
+//			text: "环状图",
+//			x:'center'
+//		},
+//		
+//	    tooltip: {
+//	        trigger: 'item',
+//	        formatter: "{a} <br/>{b}: {c} ({d}%)"
+//	    },
+//	    toolbox: {
+//	        feature: {
+//	            saveAsImage: {}
+//	        }
+//	    },
+//	    legend: {
+//	        orient: 'vertical',
+//	        x: 'left',
+//	        data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+//	    },
+//	    series: [
+//	        {
+//	            name:'访问来源',
+//	            type:'pie',
+//	            radius: ['50%', '70%'],
+//	            avoidLabelOverlap: false,
+//	            label: {
+//	                normal: {
+//	                    show: false,
+//	                    position: 'center'
+//	                },
+//	                emphasis: {
+//	                    show: true,
+//	                    textStyle: {
+//	                        fontSize: '30',
+//	                        fontWeight: 'bold'
+//	                    }
+//	                }
+//	            },
+//	            labelLine: {
+//	                normal: {
+//	                    show: false
+//	                }
+//	            },
+//	            data:[
+//	                {value:335, name:'直接访问'},
+//	                {value:310, name:'邮件营销'},
+//	                {value:234, name:'联盟广告'},
+//	                {value:135, name:'视频广告'},
+//	                {value:1548, name:'搜索引擎'}
+//	            ]
+//	        }
+//	    ]
+//	};
+//
+//annular.setOption(option);
+//})();
+//
